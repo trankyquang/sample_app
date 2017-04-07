5f589796909b0fc474bc8a4fa406b50b0313277d
Tran Ky Quang <tran.ky.quang@framgia.com>
2017-04-07 09:32:39 +0700

    Fix bug Chapter 4 lan thu nhat

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,24 +3,24 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+    @base_title = I18n.t "ruby_on_rails_tut_sample_app"
   end
 
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", (I18n.t("help") + " | " + "#{@base_title}")
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", (I18n.t("about") + " | " + "#{@base_title}")
   end
 end
