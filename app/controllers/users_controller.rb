4156c95b22f8132fb0ff3c3a2afb7d8d69b98a34
Tran Ky Quang <tran.ky.quang@framgia.com>
2017-04-10 13:00:46 +0700

    Fix bug Chapter 7 lan thu nhat

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find params[:id]
+    @user = User.find_by id: params[:id]
   end
 
   def new
@@ -13,13 +13,13 @@ class UsersController < ApplicationController
       flash[:success] = t "welcome"
       redirect_to @user
     else
-      render "new"
+      render :new
     end
   end
 
   private
-    def user_params
-      params.require(:user).permit :name, :email, :password,
-        :password_confirmation
-    end
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
+  end
 end
