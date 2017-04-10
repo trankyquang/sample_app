7e9b21657e23dfcfac9faf2e7b9582c44832fc59
Tran Ky Quang <tran.ky.quang@framgia.com>
2017-04-10 11:15:59 +0700

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
   before_save {self.email = email.downcase}
-  validates :name,  presence: true, length: {maximum: Settings.name.maximum}
+
+  validates :name, presence: true, length: {maximum: Settings.name.maximum}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: Settings.email.maximum},
     format: {with: VALID_EMAIL_REGEX},
     uniqueness: { case_sensitive: false }
-  has_secure_password
   validates :password, presence: true,
     length: {minimum: Settings.password.minimum}
+
+  has_secure_password
 end
